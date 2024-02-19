ecea4d78e7f823dd2e87105b80d4b2959a1b276b
abdelrahman zeid <zeid4643@gmail.com>
2024-02-19 13:27:00 +0200

    create app structure

---
+++ lib/core/utiles/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets{
+  static const String basePath="assets/images/";
+  static const String logo="${basePath}logo.png";
+  static const String noTask="${basePath}no_task.png";
+  static const String onOne="${basePath}on_1.png";
+  static const String onTwo="${basePath}on_2.png";
+  static const String onThree="${basePath}on_3.png";
+}
