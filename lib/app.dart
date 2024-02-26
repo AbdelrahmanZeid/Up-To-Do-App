befffac1d511006c45120ee9e3d79beb9127843c
abdelrahman zeid <zeid4643@gmail.com>
2024-02-26 13:21:57 +0200

    make app responsive and refactor code

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:notes_app/core/utiles/app_theme.dart';
 import 'package:notes_app/features/auth/presentation/views/splash_view.dart';
 
@@ -7,10 +8,21 @@ class Notes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.getAppTheme(),
-      debugShowCheckedModeBanner: false,
-      home: const SplashView(),
+    return ScreenUtilInit(
+      designSize: const Size(
+        375,
+        812,
+      ),
+      builder: (
+        _,
+        child,
+      ) {
+        return MaterialApp(
+          theme: AppTheme.getAppTheme(),
+          debugShowCheckedModeBanner: false,
+          home: const SplashView(),
+        );
+      },
     );
   }
 }
--- lib/features/task/presentation/views/add_task_view.dart
+++ lib/features/task/presentation/views/add_task_view.dart
@@ -54,186 +54,193 @@ class _AddTaskViewBodyState extends State<AddTaskViewBody> {
 
   DateTime currentDate = DateTime.now();
 
-  int currentIndex=0;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 30,
-          horizontal: 22,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              AppString.title,
-              style: AppTextStyle.latoTextStyle(
-                16,
-                FontWeight.w400,
-                AppColor.white,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: titleController,
-              decoration: const InputDecoration(
-                hintText: AppString.titleHint,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text(
-              AppString.note,
-              style: AppTextStyle.latoTextStyle(
-                16,
-                FontWeight.w400,
-                AppColor.white,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: noteController,
-              decoration: const InputDecoration(
-                hintText: AppString.noteHint,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text(
-              AppString.date,
-              style: AppTextStyle.latoTextStyle(
-                16,
-                FontWeight.w400,
-                AppColor.white,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              readOnly: true,
-              controller: dateController,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  onPressed: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      firstDate: currentDate,
-                      initialDate: currentDate,
-                      lastDate: DateTime(
-                        2025,
-                      ),
-                    );
-                    setState(() {
-                      currentDate = pickedDate!;
-                    });
-                  },
-                  icon: const Icon(
-                    Icons.calendar_today_outlined,
-                    color: AppColor.white,
-                  ),
-                ),
-                hintText: DateFormat.yMd().format(
-                  currentDate,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 30,
+            horizontal: 22,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppString.title,
+                style: AppTextStyle.latoTextStyle(
+                  16,
+                  FontWeight.w400,
+                  AppColor.white,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const StartEndTimeWidget(),
-            const SizedBox(
-              height: 5,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppString.color,
-                    style: AppTextStyle.latoTextStyle(
-                      16,
-                      FontWeight.w400,
-                      AppColor.white,
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: AppString.titleHint,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                AppString.note,
+                style: AppTextStyle.latoTextStyle(
+                  16,
+                  FontWeight.w400,
+                  AppColor.white,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: noteController,
+                decoration: const InputDecoration(
+                  hintText: AppString.noteHint,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                AppString.date,
+                style: AppTextStyle.latoTextStyle(
+                  16,
+                  FontWeight.w400,
+                  AppColor.white,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                readOnly: true,
+                controller: dateController,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        firstDate: currentDate,
+                        initialDate: currentDate,
+                        lastDate: DateTime(
+                          2025,
+                        ),
+                      );
+                      setState(() {
+                        currentDate = pickedDate!;
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.calendar_today_outlined,
+                      color: AppColor.white,
                     ),
                   ),
-                  Expanded(
-                    child: ListView.builder(
-                      // padding: EdgeInsets.symmetric(horizontal: 5,),
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (
-                        _,
-                        index,
-                      ) {
-                        Color getColor(
+                  hintText: DateFormat.yMd().format(
+                    currentDate,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const StartEndTimeWidget(),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 68,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppString.color,
+                      style: AppTextStyle.latoTextStyle(
+                        16,
+                        FontWeight.w400,
+                        AppColor.white,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        // padding: EdgeInsets.symmetric(horizontal: 5,),
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (
+                          _,
                           index,
                         ) {
-                          switch (index) {
-                            case 0:
-                              return AppColor.red;
-                            case 1:
-                              return AppColor.green;
-                            case 2:
-                              return AppColor.blueGrey;
-                            case 3:
-                              return AppColor.blue;
-                            case 4:
-                              return AppColor.orange;
-                            case 5:
-                              return AppColor.purple;
-                            default:
-                              return AppColor.grey;
+                          Color getColor(
+                            index,
+                          ) {
+                            switch (index) {
+                              case 0:
+                                return AppColor.red;
+                              case 1:
+                                return AppColor.green;
+                              case 2:
+                                return AppColor.blueGrey;
+                              case 3:
+                                return AppColor.blue;
+                              case 4:
+                                return AppColor.orange;
+                              case 5:
+                                return AppColor.purple;
+                              default:
+                                return AppColor.grey;
+                            }
                           }
-                        }
-        
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 5,
-                          ),
-                          child: GestureDetector(
-                            onTap: (){
-                               setState(() {
-                                 currentIndex=index;
-                               });
-                            },
-                            child: CircleAvatar(
-                              radius: 30,
-                              backgroundColor: getColor(
-                                index,
-                              ),
-                              child:currentIndex==index? const Icon(
-                                Icons.check,
-                                size: 30,
-                                color: AppColor.white,
-                              ):null,
+
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 5,
                             ),
-                          ),
-                        );
-                      },
-                      itemCount: 7,
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  currentIndex = index;
+                                });
+                              },
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: getColor(
+                                  index,
+                                ),
+                                child: currentIndex == index
+                                    ? const Icon(
+                                        Icons.check,
+                                        size: 30,
+                                        color: AppColor.white,
+                                      )
+                                    : null,
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: 7,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const Spacer(),
-            CustomElevatedButton(
-              color: AppColor.primary,
-              onPressed: () {},
-              text: AppString.createTask,
-              fontSize: 16,
-              btnWidth: double.infinity,
-              btnHeight: 60,
-            ),
-          ],
+              const SizedBox(
+                height:150,
+              ),
+              CustomElevatedButton(
+                color: AppColor.primary,
+                onPressed: () {},
+                text: AppString.createTask,
+                fontSize: 16,
+                btnWidth: double.infinity,
+                btnHeight: 60,
+              ),
+            ],
+          ),
         ),
       ),
     );
